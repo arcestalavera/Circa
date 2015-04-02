7dd6f7d98e9bea0a0395c11c1dcf699ed65ec592
ebackers <arcestalavera@gmail.com>
2015-04-02 16:18:25 +0800

    [SCHEMA] Latest

---
+++ circa_422015.sql
@@ -0,0 +1,303 @@
+CREATE DATABASE  IF NOT EXISTS `circa` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `circa`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: circa
+-- ------------------------------------------------------
+-- Server version	5.6.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `add_user_to_cluster`
+--
+
+DROP TABLE IF EXISTS `add_user_to_cluster`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `add_user_to_cluster` (
+  `adderID` varchar(20) DEFAULT NULL,
+  `addedID` varchar(20) DEFAULT NULL,
+  `clusterID` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `add_user_to_cluster`
+--
+
+LOCK TABLES `add_user_to_cluster` WRITE;
+/*!40000 ALTER TABLE `add_user_to_cluster` DISABLE KEYS */;
+/*!40000 ALTER TABLE `add_user_to_cluster` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `attending_an_event`
+--
+
+DROP TABLE IF EXISTS `attending_an_event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attending_an_event` (
+  `eventID` varchar(20) DEFAULT NULL,
+  `attendingID` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attending_an_event`
+--
+
+LOCK TABLES `attending_an_event` WRITE;
+/*!40000 ALTER TABLE `attending_an_event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attending_an_event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `buddy`
+--
+
+DROP TABLE IF EXISTS `buddy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `buddy` (
+  `friend_1` varchar(20) DEFAULT NULL,
+  `friend_2` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buddy`
+--
+
+LOCK TABLES `buddy` WRITE;
+/*!40000 ALTER TABLE `buddy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `buddy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cluster`
+--
+
+DROP TABLE IF EXISTS `cluster`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cluster` (
+  `clusterID` int(11) NOT NULL AUTO_INCREMENT,
+  `creatorID` varchar(20) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`clusterID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cluster`
+--
+
+LOCK TABLES `cluster` WRITE;
+/*!40000 ALTER TABLE `cluster` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cluster` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `postID` varchar(20) NOT NULL,
+  `comment` varchar(256) DEFAULT NULL,
+  `userID` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`postID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event` (
+  `eventID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `startDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `endDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `venue` varchar(45) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `hostID` varchar(20) NOT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  `eventPicture` varchar(45) NOT NULL DEFAULT 'img\\default.jpg',
+  PRIMARY KEY (`eventID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event`
+--
+
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+INSERT INTO `event` VALUES (1,'happy thursday','1992-10-27 16:00:00','1992-10-28 16:00:00','sa bahay','public','10000',NULL,'img\\default.jpg'),(2,'e','2014-12-31 17:00:00','2014-12-31 17:01:00','e','Public','10000','e','img\\default.jpg');
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invite_to_event`
+--
+
+DROP TABLE IF EXISTS `invite_to_event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invite_to_event` (
+  `hostID` varchar(20) DEFAULT NULL,
+  `eventID` varchar(20) DEFAULT NULL,
+  `invitedID` varchar(20) DEFAULT NULL,
+  `status` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invite_to_event`
+--
+
+LOCK TABLES `invite_to_event` WRITE;
+/*!40000 ALTER TABLE `invite_to_event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invite_to_event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `likes` (
+  `postID` varchar(20) DEFAULT NULL,
+  `userID` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `likes`
+--
+
+LOCK TABLES `likes` WRITE;
+/*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `likes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `postID` int(11) NOT NULL AUTO_INCREMENT,
+  `eventID` varchar(20) DEFAULT NULL,
+  `userID` varchar(20) DEFAULT NULL,
+  `postText` varchar(256) DEFAULT NULL,
+  `imgPath` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`postID`),
+  KEY `eventID_idx` (`eventID`),
+  KEY `userID_idx` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `request_to_join`
+--
+
+DROP TABLE IF EXISTS `request_to_join`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `request_to_join` (
+  `hostID` varchar(20) DEFAULT NULL,
+  `eventID` varchar(20) DEFAULT NULL,
+  `requestorID` varchar(20) DEFAULT NULL,
+  `status` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `request_to_join`
+--
+
+LOCK TABLES `request_to_join` WRITE;
+/*!40000 ALTER TABLE `request_to_join` DISABLE KEYS */;
+/*!40000 ALTER TABLE `request_to_join` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `emailAddress` varchar(45) NOT NULL,
+  `birthDate` date DEFAULT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `profilePicture` varchar(45) NOT NULL DEFAULT 'img\\default.jpg',
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10010 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (10000,'Jose','Rizal','jose_rizal@yahoo.com','1989-06-12','josejose','josephine','img\\default.jpg'),(10001,'Jose','Rizal','jose_rizal@yahoo.com','1989-06-12','josejose','josephine','img\\default.jpg'),(10002,'z','z','z',NULL,'z','z','img\\default.jpg'),(10003,'vd','vd','vd',NULL,'vd','vd','img\\default.jpg'),(10004,'','','',NULL,'','','img\\default.jpg'),(10006,'null','null','null',NULL,'null','password','img\\default.jpg'),(10007,'null','null','null',NULL,'null','password','img\\default.jpg'),(10008,'Kristian','Sisayan','kristian@yahoo.com',NULL,'kristian123','password','img\\default.jpg'),(10009,'123','123','2@2.com','2222-02-22','123','1234','img\\default.jpg');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-02 16:16:58
